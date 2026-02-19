714512ab966a2b6e1c1784a2a53d62a059a579d0
hijauinbumiagritech <hijauinbumiagritech@gmail.com>
2026-02-19 17:30:46 +0700

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,39 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"; // Baris ini ditambahkan
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "CV. Hijauin Bumi Agritech",
+  description: "Solusi Rehabilitasi Hutan, Survei Drone, dan Pengadaan Bibit Unggul",
+  icons: {
+    icon: [
+      { url: "/logo-hijauin.jpeg" },
+      { url: "/logo-hijauin.jpeg", sizes: "50x150", type: "image/jpeg" },
+    ],
+    shortcut: "/logo-hijauin.jpeg",
+    apple: "/logo-hijauin.jpeg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Analytics /> {/* Baris ini ditambahkan */}
+      </body>
+    </html>
+  );
+}
